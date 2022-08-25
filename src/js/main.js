f0e2337e131a09ab247c3eea85e5e2ed9b4158df
Evander Inacio <evander.20112@hotmail.com>
2022-08-25 13:24:37 -0300

    restart game :tada:

--- src/js/main.js
+++ src/js/main.js
@@ -2,6 +2,7 @@ const mario = document.querySelector('.mario')
 const pipe = document.querySelector('.pipe')
 
 const start = document.querySelector('.start')
+const gameOver = document.querySelector('.game-over')
 
 audioStart = new Audio('./src/audio/audio_theme.mp3')
 audioGameOver = new Audio('./src/audio/audio_gameover.mp3')
@@ -9,11 +10,28 @@ audioGameOver = new Audio('./src/audio/audio_gameover.mp3')
 
 const startGame = () => {
   pipe.classList.add('pipe-animation')
-
   start.style.display = 'none'
 
   // audio
   audioStart.play()
+}
+
+const restartGame = () => {
+  gameOver.style.display = 'none'
+  pipe.style.left = ''
+  pipe.style.right = '0'
+  mario.src = './src/img/mario.gif'
+  mario.style.width = '150px'
+  mario.style.bottom = '0'
+
+  start.style.display = 'none'
+
+  audioGameOver.pause()
+  audioGameOver.currentTime = 0;
+
+  audioStart.play()
+  audioStart.currentTime = 0;
+
 }
 
 const jump = () => {
@@ -21,39 +39,42 @@ const jump = () => {
 
   setTimeout(() => {
     mario.classList.remove('jump')
-  }, 800)
+  }, 1500)
 }
 
 const loop = () => {
   setInterval(() => {
     const pipePosition = pipe.offsetLeft
-    const marioPosition = +window
+    const marioPosition = window
       .getComputedStyle(mario)
       .bottom.replace('px', ' ')
 
-    if (pipePosition <= 120 && pipePosition > 0 && marioPosition < 111) {
-      pipe.style.animation = 'none'
+    if (pipePosition <= 120 && pipePosition > 0 && marioPosition < 100) {
+      pipe.classList.remove('.pipe-animation')
       pipe.style.left = `${pipePosition}px`
 
-      mario.style.animation = 'none'
+      mario.classList.remove('.jump')
       mario.style.bottom = `${marioPosition}px`
 
       mario.src = './src/img/game-over.png'
-      mario.style.width = '75px'
+      mario.style.width = '80px'
       mario.style.marginLeft = '50px'
-
+      
+      
       function stopAudioStart() {
         audioStart.pause()
       }
       stopAudioStart()
-
+      
       audioGameOver.play()
-
+      
       function stopAudio() {
         audioGameOver.pause()
       }
-      setTimeout(stopAudio, 8000)
-
+      setTimeout(stopAudio, 7000)
+      
+      gameOver.style.display = 'flex'
+      
       clearInterval(loop)
     }
   }, 10)
